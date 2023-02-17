a55d3121cfe45be772a27318705345ae910c0ea9
Jake Wilcox <jakewilcox242@gmail.com>
2023-02-16 19:00:06 -0700

    about me page edits

--- client/pages/Landing.tsx
+++ client/pages/Landing.tsx
@@ -1,8 +1,38 @@
-import React from "react"
+import Image from "next/image"
+import React, { useState } from "react"
 import Navbar from "../components/Navbar"
-import { motion } from "framer-motion"
+import { motion, AnimatePresence } from "framer-motion"
+import kaya from '../public/kaya.jpeg'
+import homoners from '../public/homoners.jpeg'
+import fam from '../public/fam.jpeg'
+
 
 function Landing(){
+
+    const personalPhotos = [kaya, homoners]
+    const personalText = [['About Me', "Hello World! My name is Jake Wilcox. I am 23 Years old and an aspiaring full stack developer. To learn more about my personbal life, click the button on the right. If you want to get down to buisness, hit the profesional button on the left."]]
+    var personalIndx = -1
+
+    const profesionalPhotos = []
+    const profesionalText = []
+    var profesonalIndx = -1
+
+    const [currentImage, setImage] = useState(fam)
+    const [currentAbout, setAbout] = useState(['testing', 'testing testing'])
+
+    console.log(currentAbout[0])
+    console.log(currentAbout[1])
+
+    const nextPersonal = () => {
+        personalIndx += 1
+        setAbout(personalText[personalIndx])
+        setImage(personalPhotos[personalIndx])
+    }
+
+
+
+
+
     return (
         <div>
             <div className="grid place-items-center  mt-80">
@@ -22,33 +52,57 @@ function Landing(){
 
                 <Navbar/>
 
+                <div className="bg-green-300 mt-96 mx-40 p-10">
 
-                <div className="flex mt-80 mx-40">
+                <div className="flex">
 
-                <motion.div
-                initial={{x:-100, opacity: .08}}
-                whileInView={{x:0, opacity: 1}}
-                viewport={{once:false, amount:1}}
-                transition={{duration: 2}}
-                className=" flex-1 bg-red-300">
+                    <motion.div
+                    initial={{x:-50, opacity: 0}}
+                    whileInView={{x:0, opacity: 1}}
+                    viewport={{once:false, amount:.1}}
+                    transition={{duration: 2}}
+                    className=" flex-1 bg-red-300">
 
-                    <h2 className="text-6xl">About Me</h2>
-                    <p className="flex-1 ">Hello World! My name is Jake Wilcox. I am 23 Years old and an aspiaring full stack developer</p>
-                </motion.div>
 
-                <motion.div
-                initial={{x:100, opacity: .08}}
-                whileInView={{x:0, opacity: 1}}
-                viewport={{once:false, amount:1}}
-                transition={{duration: 2}}
-                className=" flex-1 bg-blue-300">                        <p>test</p>
+                        <h2 className="text-6xl">{currentAbout[0]}</h2>
+                        <p className="">{currentAbout[1]}</p>
+
 
                     </motion.div>
+
+                    <motion.div
+                    initial={{x:50, opacity: 0}}
+                    whileInView={{x:0, opacity: 1}}
+                    viewport={{once:false, amount:.1}}
+                    transition={{duration: 2}}
+                    className="flex-2 bg-blue-300">
+
+
+
+
+                        <AnimatePresence initial={false} >
+                            <motion.div>
+                                <Image src={currentImage} alt='/'width={426}/>
+                            </motion.div>
+                        </AnimatePresence>
+                        
+                        
+
+                      
+                    
+                    </motion.div>
+                    
                     
                   
-                    </div>
+                </div>
+                <div className="flex justify-between mt-4">
 
+                <button className="bg-green-400 p-2" onClick={nextPersonal}>Professonal</button>
+                <button className="bg-green-400 p-2" onClick={nextPersonal}>Restart</button>
+                <button className="bg-green-400 p-2" onClick={nextPersonal}>Personal</button>
+                </div>
 
+                </div>
 
 
 
@@ -75,30 +129,8 @@ function Landing(){
                 <p>fdsafdsadfs</p>
                 <p>fdsafdsadfs</p>
                 <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
-                <p>fdsafdsadfs</p>
+              
+    
             </div>
     </div>
         )
