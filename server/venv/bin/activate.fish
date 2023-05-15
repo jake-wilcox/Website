5bf0511c96c86a728d1d16b5496ba6d7273385e0
Jake Wilcox <jakewilcox242@gmail.com>
2023-05-15 13:55:03 -0700

    server troubleshooting

--- server/venv/bin/activate.fish
+++ server/venv/bin/activate.fish
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/jake/repos/Website/server/venv"
+set -gx VIRTUAL_ENV "/Users/jake/code/repos/Website/server/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
