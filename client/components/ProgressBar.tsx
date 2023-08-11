0a7983d376843e89c674b6cbbbfd83242e5b1acf
Jake Wilcox <jakewilcox242@gmail.com>
2023-08-10 19:15:22 -0700

    typo

--- client/components/ProgressBar.tsx
+++ client/components/ProgressBar.tsx
@@ -1,29 +1,27 @@
 import React from "react"
-import Link from "next/link"
-import { motion} from "framer-motion"
-import { useEffect, useState} from "react";
+import { motion } from "framer-motion"
 
 type Props = {
     proficentcy: number
 }
 
 
-const ProgressBar = ({proficentcy}: Props) => {
+const ProgressBar = ({ proficentcy }: Props) => {
 
     const proficentcyPx = (proficentcy / 10) * 300
 
     return (
 
-            <div className="bg-slate-200 w-[300px] h-5 rounded-full flex items-center">
-                <motion.div initial={{width: 0}} animate={{width: proficentcyPx}} transition={{duration:2}}
+        <div className="bg-slate-200 w-[300px] h-5 rounded-full flex items-center">
+            <motion.div initial={{ width: 0 }} animate={{ width: proficentcyPx }} transition={{ duration: 2 }}
                 className="bg-dankRed h-4 w-1/2 rounded-full flex m-1">
-                    
-                    
-                </motion.div>
-            </div>
 
 
-        )
+            </motion.div>
+        </div>
+
+
+    )
 }
 
 export default ProgressBar
